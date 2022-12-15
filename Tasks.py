3755c0b39a97a30aecfc3c783327be17f74bc34e
QA Murat Tas <itmmt38@gmail.com>
2022-12-15 14:31:02 +0300

    python

---
+++ Tasks.py
@@ -0,0 +1,40 @@
+"""
+1- Bir müşterinin Aşağıdaki bilgileri için değişken oluştrunuz
+
+Müşteri Adı
+Müşteri Soyadı
+Müşteri Ad+soyad
+Müşterş cinsiyet
+Müşteri tc Kimlik
+Müşteri Doğum yılı
+Müşteri Adres Bilgisi
+Müşteri Yaşı
+
+"""
+musteriAdi = "Murat"
+musteriSoyadi ="Taş"
+msuteriAdSoyad = musteriAdi+ " " +musteriSoyadi
+musteriCinsiyet = True # erkek
+musteriTckimlik ="12345678901"
+musteriDogumYili= 1982
+musteriAdresi="İstanbul Üsküdar"
+musteriYssi=2022- musteriDogumYili
+
+print(musteriYssi)
+
+"""
+2- aşağıdaki siparişelrin toplamı bilgisini hesaplayınız
+
+ Sipariş 1 => 110 tl
+ Sipariş 2 => 1100.5 tl
+ Sipariş 3 =>356.95 tl
+
+"""
+order1 =110
+order2 =1100.5
+order3=356.95
+total = order1+order2+order3
+
+print ("Total :",total)
+
+
---
+++ degiskenler.py
@@ -0,0 +1,44 @@
+maasAli=5000
+maasAhmet=4000
+vergi=.27
+
+print(maasAli-(maasAli*vergi))
+print(maasAhmet-(maasAhmet*vergi))
+
+print (5000 - (5000*.27))
+print (4000-(4000*0.27))
+# Değişkne Tanımlama
+# rakam ile başlayamaz
+
+number1=10
+print (number1)
+number1=20
+print(number1)
+number1 +=30
+print(number1)
+
+#byük küçük harf duyarlılığı vardır
+
+age = 20
+AGe = 40
+print(age)
+print(AGe)
+
+# Türkçekarakter kullanma
+yas =20
+_age =20
+
+x=1                     #int
+y=2.3                   #float
+name = "Çınar"          #string
+isStudent =True         #bool
+
+# x,y,name, isStudent =(1,2.3,"Çınar",True) tek satırda atam yaptık
+a="10"
+b= "20"
+print(a+b) # 1020 string concat
+
+firstName = "sadık"
+lastNaame ="Turan"
+
+print(firstName+lastNaame)
